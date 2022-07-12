2cdee36f8a2654161e36811b5884d3b5e223d3d8
zhaozhenyuan <zhaoyihuaer@aliyun.com>
2022-07-12 18:29:02 +0800

    添加测试文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: 'Zh\'s 小记',
     description: '',
     //base: '/',    // 部署的域名路径配置
-    //dest: "./dist", // 打包后的输出目录
+    //dest: "./dist", // 打包后的输出目录 注意要与git action 自动部署文件的 build dir一致
     // head设置
     head: [
         [
@@ -31,13 +31,13 @@ module.exports = {
                 "link": "/",
                 "icon": "reco-home"
             },
+            // {
+            //     "text": "TimeLine",
+            //     "link": "/timeline/",
+            //     "icon": "reco-date"
+            // },
             {
-                "text": "TimeLine",
-                "link": "/timeline/",
-                "icon": "reco-date"
-            },
-            {
-                "text": "Docs",
+                "text": "文档",
                 "icon": "reco-message",
                 "items": [
                     {
@@ -47,12 +47,12 @@ module.exports = {
                 ]
             },
             {
-                "text": "GitHub",
+                "text": "联系",
                 "icon": "reco-message",
                 "items": [
                     {
                         "text": "GitHub",
-                        "link": "https://github.com/recoluan",
+                        "link": "https://github.com/zhzy-to",
                         "icon": "reco-github"
                     }
                 ]
@@ -71,7 +71,7 @@ module.exports = {
             },
             "tag": {
                 "location": 3,
-                "text": "Tag"
+                "text": "标签"
             }
         },
         "friendLink": [
@@ -123,40 +123,30 @@ module.exports = {
                         artist: 'H.Q / ZebraZebra',
                         url:'/bgm/Nutag.m4a',
                         cover:'/bgm/Nutag.jpg',
-                        //url: 'https://m801.music.126.net/20220712151420/f024f9365172d19a72d1e2607bafa190/jdyyaac/5559/5352/065c/b36e12bd2bd8972ac05b16c556806a95.m4a',
-                        //cover: 'https://p2.music.126.net/KRB5QvbzVvcsnvYVIuJVIw==/109951163292383888.jpg?param=130y130'
                     },
                     {
                         name: 'Fly',
                         artist: 'ANU',
                         url:'/bgm/Fly.m4a',
                         cover:'/bgm/Fly.jpg',
-                        //url: 'https://m701.music.126.net/20220712152838/d09ab4797d4d36af6b00cb3bc09937a5/jdyyaac/0759/520e/0053/033f01e2ceedf53aa4c3799c520713ed.m4a',
-                        //cover: 'https://p1.music.126.net/yp8qCRkDQj2NzQ68hzhEJA==/19145795974603349.jpg?param=130y130'
                     },
                     {
                         name: 'Hello World',
                         artist: 'Alan Walker / Torine',
                         url:'/bgm/Hello World.m4a',
                         cover:'/bgm/Hello World.jpg',
-                        //url: 'https://m701.music.126.net/20220712153628/b12fd501aaaa693a7c671392413aeed5/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/12592097549/b722/4bc1/f19b/a44c8b75a1eb342024e81131db4354e4.m4a',
-                        //cover: 'https://p1.music.126.net/yp8qCRkDQj2NzQ68hzhEJA==/19145795974603349.jpg?param=130y130'
                     },
                     {
                         name: '锡尼河畔',
                         artist: '阿斯罕 / 吴兰塔娜',
                         url:'/bgm/xinihepan.m4a',
                         cover:'/bgm/xinihepan.jpg',
-                        //url: 'https://m10.music.126.net/20220712155009/d906958bd47124b0f3b224f071ae9dc7/yyaac/055c/535d/5358/267d71af71d70b054d4ad2e18f4e4101.m4a',
-                        //cover: 'https://p2.music.126.net/-6OpZwJwUuW2FbFTQyuqaQ==/109951164399125591.jpg?param=130y130'
                     },
                     {
                         name: 'Here And Now',
                         artist: 'Paxel',
                         url:'/bgm/Here And Now.m4a',
                         cover:'/bgm/Here And Now.jpg',
-                        //url: 'https://m10.music.126.net/20220712162532/773da050537aed4441a2979a9ba4749b/yyaac/obj/wonDkMOGw6XDiTHCmMOi/3943770218/61a8/f397/9fb7/b2ce4d276a5f28e6581e09abfad29eba.m4a',
-                        //cover: 'https://p1.music.126.net/2RuDEzqA0_dFA6d0pS_nmQ==/109951163220268056.jpg?param=130y130'
                     },
                 ] ,
                 // 是否默认缩小
--- docs/go/README.md
+++ docs/go/README.md
@@ -1,14 +1,5 @@
----
-title: 烤鸭的做法1
-date: '2019-08-08 08:00:00'
-sidebar: 'auto'
-categories:
- - 烹饪 
- - 爱好
-tags:
- - 1
- - 2
-publish: true
----
 
-# go
+ <!-- 每个目录下单独设置readme文件 作为该下文件锚点 -->
+
+[类型断言](./类型断言.md)
+
